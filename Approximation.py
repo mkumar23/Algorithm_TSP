224128b996736ed8f81cb75b8d01c17d3661beb6
ysong340 <songyangustc@gmail.com>
2014-11-30 16:53:49 +0800

    Update Approximation.py

--- Approximation.py
+++ Approximation.py
@@ -10,18 +10,24 @@ def dfs(G, i):
     for s,t,w in G.edges(i,data=True):
         if visited[t - 1] == False:
             dfs(G, t)
-
+	
 start_time = time.time()
 G, optimal = readData.createGraph(sys.argv[1])
 T, non = prim.primMST(G)
 
 visited = [False] * len(T)
 record = []
-dfs(T, 1)
-#print record
-length = 0
-for i in xrange(len(record)-1):
-    length += G.get_edge_data(record[i], record[i+1])['weight']
 
-total_time = (time.time() - start_time)
-print total_time, length, optimal, float(length - int(optimal))/float(optimal)
+def main():
+	dfs(T, 1)
+	#print record
+	length = 0
+	for i in xrange(len(record)-1):
+	    length += G.get_edge_data(record[i], record[i+1])['weight']
+	length = length + G.get_edge_data(record[0], record[-1])['weight']
+
+	total_time = (time.time() - start_time)
+	print total_time, length, optimal, float(length - int(optimal))/float(optimal)
+	return record, length
+
+print main()
